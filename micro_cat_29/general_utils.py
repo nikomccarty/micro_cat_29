e36ccd0359839f973f3291b32e59173f9e456ac6
Niko McCarty <nmccarty@caltech.edu>
2019-12-11 13:09:03 -0800

    update to general_utils

--- micro_cat_29/general_utils.py
+++ micro_cat_29/general_utils.py
@@ -1,2 +1,13 @@
 import numpy as np
 import pandas as pd
+
+def draw_bs_sample(data):
+    """Draw a bootstrap sample from a 1D data set."""
+    return np.random.choice(data, size=len(data))
+
+def draw_bs_reps_mean(data, size=1):
+    """Draw boostrap replicates of the mean from 1D data set."""
+    out = np.empty(size)
+    for i in range(size):
+        out[i] = np.mean(draw_bs_sample(data))
+    return out
